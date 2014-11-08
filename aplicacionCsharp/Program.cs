3cb30b9fb6762bfb7d125e1ae278d32d340323b3
Patricio1 <apatriciol_@hotmail.com>
2014-11-08 16:57:16 -0500

    commit from github.com

--- aplicacionCsharp/Program.cs
+++ aplicacionCsharp/Program.cs
@@ -16,6 +16,7 @@ namespace aplicacionCsharp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //hola mundo
         }
     }
 }
